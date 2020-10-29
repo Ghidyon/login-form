d18b372b654b72b840a61ab1612c4a059a0b9d7f
Ghidyon <gideonakunana@gmail.com>
2020-10-29 18:10:26 +0100

    validations modified

--- script.js
+++ script.js
@@ -11,8 +11,51 @@ class FormValidator {
 
     validateInput(field) {
         if (field.value.trim() === "") {
-            return this.setStatus(field, `${field.getAttribute('name')} cannot be blank!`, "error");
+            this.setStatus(field, `${field.getAttribute('name')} cannot be blank!`, "error");
         }
+        else {
+            if (field.type === "text") {
+                const matchText = /^[a-zA-Z\s][^0-9]+$/;
+                if (matchText.test(field.value)) {
+                    this.setStatus(field, null, "success");
+                }
+                else {
+                    this.setStatus(field, "Please don't use number characters.", "error");
+                }
+            }
+
+            if (field.type === "email") {
+                const matchEmail = /\S+@\S+.\S+/;
+                if (matchEmail.test(field.value)) {
+                    this.setStatus(field, null, "success");
+                }
+                else {
+                    this.setStatus(field, "Please input a valid email.", "error");
+                }
+            }
+
+            if (field.id === "password") {
+                if (field.value.length < 8) {
+                    this.setStatus(field, "Password should be 8 characters long.", "error");
+                }
+                else {
+                    this.setStatus(field, null, "success");
+                }
+            }
+
+            if (field.id === "confirm-password") {
+                const password = this.form.querySelector('#password');
+
+                if (field.value !== password.value) {
+                    this.setStatus(field, "Password does not match!", "error");
+                }
+                else {
+                    this.setStatus(field, null, "success");
+                }
+            }
+        }
+
+
     }
 
     setStatus(field, message, status) {
